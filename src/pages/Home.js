f43de681c348bdf3959a81a5866be3773d2022ef
Shahriar Swim <shahriar2k18@gmail.com>
2021-02-16 20:56:56 +0600

    added game details modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { loadGames } from "../actions/gamesAction";
 //Components
 import Game from "../components/Game";
+import GameDetail from "../components/GameDetail";
 
 const Home = () => {
   //FETCH GAMES
@@ -18,6 +19,7 @@ const Home = () => {
   const { popular, newGames, upcoming } = useSelector((state) => state.games);
   return (
     <GameList>
+      <GameDetail />
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
